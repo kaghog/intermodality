0751520db926926b40e97895baf704f5239b04d0
kaghog <grace.kagho@ivt.baug.ethz.ch>
2022-09-27 21:53:13 +0200

    fix error

--- src/main/java/simulation/utils/CreateTripBasedPopulation.java
+++ src/main/java/simulation/utils/CreateTripBasedPopulation.java
@@ -30,8 +30,6 @@ public class CreateTripBasedPopulation {
         /*String population = "/home/kaghog/projects/scenarios/zurich/zurich_population_1pct.xml.gz";
         String outputfile = "/home/kaghog/projects/scenarios/zurich/zurich_population_1pct_ptTripsgreater1km.xml.gz";*/
 
-        BufferedWriter writer = IOUtils.getBufferedWriter("/home/kaghog/projects/scenarios/zurich/zurich_population_1pct_ptTripsgreater1km.txt");
-
 
         Config config = ConfigUtils.createConfig();
         Scenario scenario = ScenarioUtils.createScenario(config);
@@ -46,7 +44,6 @@ public class CreateTripBasedPopulation {
         Population newPop = PopulationUtils.createPopulation(scenario.getConfig());
 
         //filter out trips less than 1km
-        int m =1;
         for (final Person person : scenario.getPopulation().getPersons().values()) {
             List<TripStructureUtils.Trip> trips = TripStructureUtils.getTrips(person.getSelectedPlan().getPlanElements());
             int n = 11;
@@ -54,15 +51,17 @@ public class CreateTripBasedPopulation {
             for (TripStructureUtils.Trip trip : trips) {
                 double distance = CoordUtils.calcEuclideanDistance(trip.getOriginActivity().getCoord(), trip.getDestinationActivity().getCoord());
                 if (distance >= 1000) {
-                    Person newPerson = populationFactory.createPerson(Id.create(person.getId().toString() + n, Person.class));
-                    Plan p = populationFactory.createPlan();
 
-
-                    //add first activity to plan
-                    p.addActivity(trip.getOriginActivity());
 
                     //remove the route and set the mode
                     for (Leg leg : trip.getLegsOnly()) {
+
+                        Person newPerson = populationFactory.createPerson(Id.create(person.getId().toString() + n, Person.class));
+                        Plan p = populationFactory.createPlan();
+
+                        //add first activity to plan
+                        p.addActivity(trip.getOriginActivity());
+
                         if (leg.getRoute() != null) {
                             leg.setRoute(null);
                         }
@@ -70,50 +69,46 @@ public class CreateTripBasedPopulation {
                         leg.getAttributes().putAttribute("routingMode", leg.getMode());
 
                         p.addLeg(leg);
+
+
+                        //add second activity
+                        p.addActivity(trip.getDestinationActivity());
+
+                        //add plan to person
+                        newPerson.addPlan(p);
+
+                        //add the person attributes
+                        newPerson.getAttributes().putAttribute("age", person.getAttributes().getAttribute("age"));
+                        newPerson.getAttributes().putAttribute("bikeAvailability", person.getAttributes().getAttribute("bikeAvailability"));
+                        newPerson.getAttributes().putAttribute("carAvail", person.getAttributes().getAttribute("carAvail"));
+                        newPerson.getAttributes().putAttribute("employed", person.getAttributes().getAttribute("employed"));
+                        newPerson.getAttributes().putAttribute("hasLicense", person.getAttributes().getAttribute("hasLicense"));
+                        newPerson.getAttributes().putAttribute("home_x", person.getAttributes().getAttribute("home_x"));
+                        newPerson.getAttributes().putAttribute("home_y", person.getAttributes().getAttribute("home_y"));
+                        newPerson.getAttributes().putAttribute("isCarPassenger", person.getAttributes().getAttribute("isCarPassenger"));
+                        newPerson.getAttributes().putAttribute("isOutside", person.getAttributes().getAttribute("isOutside"));
+                        newPerson.getAttributes().putAttribute("mzHeadId", person.getAttributes().getAttribute("mzHeadId"));
+                        newPerson.getAttributes().putAttribute("mzPersonId", person.getAttributes().getAttribute("mzPersonId"));
+                        newPerson.getAttributes().putAttribute("ptHasGA", person.getAttributes().getAttribute("ptHasGA"));
+                        newPerson.getAttributes().putAttribute("ptHasHalbtax", person.getAttributes().getAttribute("ptHasHalbtax"));
+                        newPerson.getAttributes().putAttribute("ptHasStrecke", person.getAttributes().getAttribute("ptHasStrecke"));
+                        newPerson.getAttributes().putAttribute("ptHasVerbund", person.getAttributes().getAttribute("ptHasVerbund"));
+                        newPerson.getAttributes().putAttribute("spRegion", person.getAttributes().getAttribute("spRegion"));
+                        newPerson.getAttributes().putAttribute("statpopHouseholdId", person.getAttributes().getAttribute("statpopHouseholdId"));
+                        newPerson.getAttributes().putAttribute("statpopPersonId", person.getAttributes().getAttribute("statpopPersonId"));
+
+                        //add original personID
+                        newPerson.getAttributes().putAttribute("origPersonId", person.getId().toString());
+
+                        newPop.addPerson(newPerson);
+                        n++;
                     }
-
-                    //add second activity
-                    p.addActivity(trip.getDestinationActivity());
-
-                    //add plan to person
-                    newPerson.addPlan(p);
-
-                    //add the person attributes
-                    newPerson.getAttributes().putAttribute("age", person.getAttributes().getAttribute("age"));
-                    newPerson.getAttributes().putAttribute("bikeAvailability", person.getAttributes().getAttribute("bikeAvailability"));
-                    newPerson.getAttributes().putAttribute("carAvail", person.getAttributes().getAttribute("carAvail"));
-                    newPerson.getAttributes().putAttribute("employed", person.getAttributes().getAttribute("employed"));
-                    newPerson.getAttributes().putAttribute("hasLicense", person.getAttributes().getAttribute("hasLicense"));
-                    newPerson.getAttributes().putAttribute("home_x", person.getAttributes().getAttribute("home_x"));
-                    newPerson.getAttributes().putAttribute("home_y", person.getAttributes().getAttribute("home_y"));
-                    newPerson.getAttributes().putAttribute("isCarPassenger", person.getAttributes().getAttribute("isCarPassenger"));
-                    newPerson.getAttributes().putAttribute("isOutside", person.getAttributes().getAttribute("isOutside"));
-                    newPerson.getAttributes().putAttribute("mzHeadId", person.getAttributes().getAttribute("mzHeadId"));
-                    newPerson.getAttributes().putAttribute("mzPersonId", person.getAttributes().getAttribute("mzPersonId"));
-                    newPerson.getAttributes().putAttribute("ptHasGA", person.getAttributes().getAttribute("ptHasGA"));
-                    newPerson.getAttributes().putAttribute("ptHasHalbtax", person.getAttributes().getAttribute("ptHasHalbtax"));
-                    newPerson.getAttributes().putAttribute("ptHasStrecke", person.getAttributes().getAttribute("ptHasStrecke"));
-                    newPerson.getAttributes().putAttribute("ptHasVerbund", person.getAttributes().getAttribute("ptHasVerbund"));
-                    newPerson.getAttributes().putAttribute("spRegion", person.getAttributes().getAttribute("spRegion"));
-                    newPerson.getAttributes().putAttribute("statpopHouseholdId", person.getAttributes().getAttribute("statpopHouseholdId"));
-                    newPerson.getAttributes().putAttribute("statpopPersonId", person.getAttributes().getAttribute("statpopPersonId"));
-
-                    //add original personID
-                    newPerson.getAttributes().putAttribute("origPersonId", person.getId().toString());
-
-                    newPop.addPerson(newPerson);
-
-                    m++;
                 }
-                n++; //placed here instead of under the filtering allows one to know the trip stage
 
             }
         }
-        System.out.println(m);
-        writer.flush();
-        writer.close();
 
-        new PopulationWriter(newPop).write(outputfile);
+        new PopulationWriter(newPop).writeV4(outputfile);
 
         System.out.println("Finished");
 
